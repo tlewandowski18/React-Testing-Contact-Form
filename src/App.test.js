773da737e0d77aa2203212b8b77762af87bfaac9
tlewandowski18 <tlewandowski18@gmail.com>
2020-02-23 19:20:39 -0500

    add tests to app and contact form components

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,9 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const { getByText } = render(<App />);
+
+  const contact = getByText(/contact/i)
+
+  expect(contact).toBeInTheDocument()
 });
